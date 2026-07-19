fix depth normalization as GeoNT input

src/geont/models/geont.py
@@ -341,8 +341,7 @@ class GeoNTWrapper(nn.Module):
                 "depth": scaled_depth[fi],  # [H,W]
                 "mask": train_depth_valid[fi],  # [H,W]
             }
-            target_scale_ratio = scale[jj[mask]] / scale[fi]
-            target_depth = scaled_depth[jj[mask]] * target_scale_ratio[:, None, None]
+            target_depth = scaled_depth[jj[mask]]
             target_depth_input = {
                 "depth": target_depth,  # [E_i,H,W], normalized in source/reference gauge.
                 "mask": train_depth_valid[jj[mask]],  # [E_i,H,W]

src/geont_runtime/slam/components/measurements.py
@@ -244,8 +244,8 @@ class GeoNTMeasurements:
         intrinsics = self.buffer.intrinsics[0:1].expand(ii.numel(), -1).contiguous()
         source_depth = self.buffer.depths_sens_normed[ii, 0].float()
         source_mask = self.buffer.non_sky_masks[ii, 0]
-        target_scale_ratio = self.buffer.depths_sens_scale[jj, 0].float() / self.buffer.depths_sens_scale[ii, 0].float()
-        target_depth = self.buffer.depths_sens_normed[jj, 0].float() * target_scale_ratio[:, None, None]
+        # target_scale_ratio = self.buffer.depths_sens_scale[jj, 0].float() / self.buffer.depths_sens_scale[ii, 0].float()
+        target_depth = self.buffer.depths_sens_normed[jj, 0].float()
         target_mask = self.buffer.non_sky_masks[jj, 0]
         zero_flow = torch.zeros_like(flow)
         zero_info = torch.zeros_like(info)
@@ -281,8 +281,8 @@ class GeoNTMeasurements:
         target_intrinsics = self.buffer.intrinsics[0:1].expand(neighbors.numel(), -1).contiguous()
         source_depth = keyframe_candidate.depth_sens_normed[0].float()
         source_mask = keyframe_candidate.mask[0]
-        target_scale_ratio = self.buffer.depths_sens_scale[neighbors, 0].float() / keyframe_candidate.scale[0].float()
-        target_depth = self.buffer.depths_sens_normed[neighbors, 0].float() * target_scale_ratio[:, None, None]
+        # target_scale_ratio = self.buffer.depths_sens_scale[neighbors, 0].float() / keyframe_candidate.scale[0].float()
+        target_depth = self.buffer.depths_sens_normed[neighbors, 0].float()
         target_mask = self.buffer.non_sky_masks[neighbors, 0]
         measurement = self.predict_pose_depth_impl(
             ii,
@@ -313,8 +313,8 @@ class GeoNTMeasurements:
         target_intrinsics = self.buffer.intrinsics[0:1].expand(neighbors.numel(), -1).contiguous()
         source_depth = self.buffer.depths_sens_normed[source, 0].float()
         source_mask = self.buffer.non_sky_masks[source, 0]
-        target_scale_ratio = self.buffer.depths_sens_scale[neighbors, 0].float() / self.buffer.depths_sens_scale[source, 0].float()
-        target_depth = self.buffer.depths_sens_normed[neighbors, 0].float() * target_scale_ratio[:, None, None]
+        # target_scale_ratio = self.buffer.depths_sens_scale[neighbors, 0].float() / self.buffer.depths_sens_scale[source, 0].float()
+        target_depth = self.buffer.depths_sens_normed[neighbors, 0].float()
         target_mask = self.buffer.non_sky_masks[neighbors, 0]
         return self.predict_pose_depth_impl(
             ii,

training.py
@@ -6,7 +6,6 @@ import time
 import math
 from pathlib import Path
 from typing import Sized
-from collections import OrderedDict
 
 import hydra
 import torch
@@ -190,12 +189,7 @@ def train_one_epoch(model: torch.nn.Module, criterion: torch.nn.Module,
         # poses w2c
         images, poses, depths, depths_valid, intrinsics = [x.to(device) for x in batch]
 
-        if np.random.rand() < 0.5:
-            graph = build_frame_graph(poses, 1.0 / depths, intrinsics, num=args.edges)
-        else:
-            graph = OrderedDict()
-            for i in range(args.n_frames):
-                graph[i] = [j for j in range(args.n_frames) if i!=j and abs(i-j) <= 2]
+        graph = build_frame_graph(poses, 1.0 / depths, intrinsics, num=args.edges)
 
         prediction = model(
             images,